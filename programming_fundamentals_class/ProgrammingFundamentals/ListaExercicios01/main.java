5c61ab00b20a66d55928828e828bd1b8b9fa0f3b
EDUARDO ALCARIA LOPES <e.alcaria@edu.pucrs.br>
2025-03-07 19:37:43 -0300

    1st fundamental programming pratical class

---
+++ programming_fundamentals_class/ProgrammingFundamentals/ListaExercicios01/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        long i = 0;
+        while (true){
+            System.out.print("\n"+i);
+            i++;
+        }
+    }
+}
