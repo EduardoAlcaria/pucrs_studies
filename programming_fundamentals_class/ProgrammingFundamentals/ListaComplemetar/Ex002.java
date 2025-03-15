972464ab92be1f4bf14e08c6f4f164d9bb5e658d
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-03-15 12:52:20 -0300

    completed all exercises from the complementary list

--- programming_fundamentals_class/ProgrammingFundamentals/ListaComplemetar/Ex002.java
+++ programming_fundamentals_class/ProgrammingFundamentals/ListaComplemetar/Ex002.java
@@ -1,51 +1,65 @@
 package ListaComplemetar;
 
-import java.util.Arrays;
+/*
+  2. Na ginástica olímpica, a nota de um atleta é a soma das notas de duas bancas de
+  árbitros: banca A e banca B. A nota da banca A é a nota de partida do atleta. A nota da
+  banca B é calculada da seguinte forma:
+  (a) 6 juízes atribuem notas ao atleta;
+  (b) a nota mais baixa e a mais alta são descartadas;
+  (c) é feita a média das notas restantes.
+ */
+
 import java.util.Scanner;
 
 public class Ex002 {
     public static void main(String[] args) {
-        double judge1B, judge2B, judge3B;
-        double judge4B, judge5B, judge6B;
+        double judge1B, judge2B, judge3B, judge4B, judge5B, judge6B;
 
         double fixA = 10;
 
-        double totalScore = 0;
+        double max;
+        double min;
 
-        double[] scores;
+        double totalScore;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1st score for the A judges: ");
+        System.out.println("1st score for the B judges: ");
         judge1B = scanner.nextInt();
 
-        System.out.println("2nd score for the A judges: ");
+        System.out.println("2nd score for the B judges: ");
         judge2B = scanner.nextInt();
 
-        System.out.println("3th score for the A judges: ");
+        max = Math.max(judge1B, judge2B);
+        min = Math.min(judge1B, judge2B);
+
+        System.out.println("3th score for the B judges: ");
         judge3B = scanner.nextInt();
+
+        max = Math.max(max, judge3B);
+        min = Math.min(min, judge3B);
 
         System.out.println("1st score for the B judges: ");
         judge4B = scanner.nextInt();
 
+        max = Math.max(max, judge4B);
+        min = Math.min(min, judge4B);
+
         System.out.println("2nd score for the B judges: ");
         judge5B = scanner.nextInt();
+
+        max = Math.max(max, judge5B);
+        min = Math.min(min, judge5B);
 
         System.out.println("3th score for the B judges: ");
         judge6B = scanner.nextInt();
 
+        max = Math.max(max, judge6B);
+        min = Math.min(min, judge6B);
+
         scanner.close();
 
-        scores = new double[]{judge1B, judge2B, judge3B, judge4B, judge5B, judge6B};
-        Arrays.sort(scores);
-
-        double min = scores[0];
-        double max = scores[5];
-
-
-        for (int i = 1; i < 5; i++) {
-            totalScore += scores[i];
-        }
+        totalScore = (judge1B + judge2B + judge3B + judge4B + judge5B + judge6B) - max - min;
 
         double finalScore = (totalScore / 4) + fixA;
         System.out.println("max over: " + max);
