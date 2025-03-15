71696e4c12ba4ff503a3acaad2ee761d78fb678e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-03-15 13:19:05 -0300

    1st exercise from the 2nd exercise list done

---
+++ programming_fundamentals_class/ProgrammingFundamentals/ListaExercicios02/Ex001.java
@@ -0,0 +1,19 @@
+package ListaExercicios02;
+
+
+import java.util.Scanner;
+
+public class Ex001 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("type your age: ");
+        int age = scan.nextInt();
+
+        if (age >= 18) {
+            System.out.println("you are an adult");
+        }else {
+            System.out.println("you are not an adult");
+        }
+    }
+}
